f28acb33688d7b1482fe46d104372a0fa19d31f4
vagnerlands <vagnerlands@gmail.com>
2020-04-18 13:16:13 -0700

    [chore] tank is starting to move, also sorted out several bugs on blueprint.

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -3,3 +3,12 @@
 
 #include "TankTrack.h"
 
+void UTankTrack::SetThrottle(float Throttle)
+{
+	UE_LOG(LogTemp, Warning, TEXT(" %s was set to %f "), *GetOwner()->GetName(), Throttle);
+
+	FVector ForceApplied = GetForwardVector() * Throttle* TrackMaxDrivingForce;
+	FVector ForceLocation = GetComponentLocation();
+	UPrimitiveComponent* TankRoot = Cast<UPrimitiveComponent>(  GetOwner()->GetRootComponent() );
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
--- BattleTank/Source/BattleTank/TankTrack.h
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -9,9 +9,17 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(meta = (BlueprintSpawnableComponent), ClassGroup=(Custom))
 class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
-	
+public:
+    // Apply force on track throttle, this will cause the tank to turn to the track side.
+    UFUNCTION(BlueprintCallable, Category=Input)
+    void SetThrottle(float Throttle);
+
+    // Set the maximum driving force that will be applied on the tank which
+    // will make it move
+    UPROPERTY(EditAnywhere)
+    float TrackMaxDrivingForce = 200000.f;
 };
